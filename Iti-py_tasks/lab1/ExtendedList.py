3902476016865a62c0a8635a154cb41bab581351
ahmed <166109035+ahmed-zaki-dev@users.noreply.github.com>
2026-07-27 15:16:40 +0300

    Delete Iti-py_tasks/lab1/ExtendedList.py

--- Iti-py_tasks/lab1/ExtendedList.py
+++
@@ -1,8 +0,0 @@
-#Given two lists lst1 and lst2, your task is to return a list formed by 
-#the elements of lst1 followed by the elements of lst2
-
-list1 = [1, 2, 3]
-list2 = [4, 5, 6]
-list1.extend(list2)
-
-print(list1)
