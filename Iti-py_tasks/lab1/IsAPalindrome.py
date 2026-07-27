fe5ab6174543184453b627ba0dc09129cfaf029d
ahmed <166109035+ahmed-zaki-dev@users.noreply.github.com>
2026-07-27 15:16:55 +0300

    Delete Iti-py_tasks/lab1/IsAPalindrome.py

--- Iti-py_tasks/lab1/IsAPalindrome.py
+++
@@ -1,8 +0,0 @@
-#Given the string, check if it is a palindrome. 
-#Example For inputString = "aabaa", the output should be solution(inputString) = true;
-#For inputString = "abac", the output should be solution(inputString) = false;
-#For inputString = "a", the output should be solution(inputString) = true.
-
-while True:
-    usr_input = input("Enter your String: ")
-    print(usr_input == usr_input[::-1])
